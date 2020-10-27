4d38585cb4f19416bf7827b5933e6984c08603bc
Juan Gimenez <j.martin.utn@gmail.com>
2020-10-27 15:57:14 -0300

    startig with the chat component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './App.module.css'
 
+import Header from './Header'
+import ChatSection from './ChatSection'
+import SendButton from './SendButton'
+
 export default function App({
   company_name = 'Ann0nIT',
   txt_reply_time = 'Typically replies within a day',
@@ -8,5 +12,11 @@ export default function App({
   What can we do for you?`,
   txt_button = 'Send'
 }) {
-  return <div className={styles.root}>content</div>
+  return (
+    <div className={styles.root}>
+      <Header />
+      <ChatSection />
+      <SendButton />
+    </div>
+  )
 }
