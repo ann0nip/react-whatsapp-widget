68115e572b2d757216978b34ab7e01956b4838ed
Micheal Ndondo <70109131+nd0nd0@users.noreply.github.com>
2023-10-28 12:06:35 +0300

    issue #26 : Change widget position
    From a UX point of view, it is recommended to keep chat icons on the bottom right corner of your website, as this is a familiar and usual spot where people naturally notice and navigate.
    
    However, UX is subjective. This PR added support issue #26 .
    
    I would recommend the WA button to have a high contrast background `#5ad167` and the WA chat icon in `white` in light mode situations and vice versa --> issue #27

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, Fragment, useEffect } from 'react';
-
 import { ChatContext } from '../../contexts/chat.context';
 import ChatBox from '../chat-box/chat-box.component';
 import Header from '../header/header.component';
@@ -8,6 +7,7 @@ import WaButton from '../wa-button/wa-button.component';
 import styles from './app.module.css';
 
 const App = (props) => {
+    console.log("🚀 ~ file: app.component.jsx:10 ~ App ~ props:", props)
     const { isChatOpen, setIsChatOpen } = useContext(ChatContext);
     useEffect(() => {
         if (props.open) {
@@ -20,13 +20,14 @@ const App = (props) => {
             <div
                 className={`${styles.root} ${
                     isChatOpen ? styles.open : styles.close
-                }`}
+                } ${props.position === 'left' ? styles.positionLeft : styles.positionRight} ` }
             >
+
                 <Header {...props} />
                 <ChatBox {...props} />
                 <SendButton {...props} />
             </div>
-            <WaButton />
+            <WaButton {...props} />
         </Fragment>
     );
 };
